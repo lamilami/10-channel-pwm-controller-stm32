24f7277713c900d74f15ff79c45339b9136aabe2
takemitsutoru@gmail.com <takemitsutoru@gmail.com@f55d1f62-bbc9-130b-3bdc-42405c7f4519>
2012-07-07 13:50:01 +0000

    第一次上传

---
+++ trunk/User/main.c
@@ -0,0 +1,315 @@
+// STM32 4-channel Servo Demo for 24 MHz VL Discovery - sourcer32@gmail.com
+ 
+#include "stm32F10x.h"
+#include "delay.h"
+
+void RCC_Configuration(void);
+void GPIO_Configuration(void);
+void NVIC_Configuration(void);
+void TIM3_Configuration(void);
+ 
+ 
+volatile float current_angle[10] = {0,0,0,0,0,0,0,0,0,0}; // +/- 90 degrees, use floats for fractional
+volatile float delta_angle[10]={0,0,0,0,0,0,0,0,0};
+u32 left_step[10]={0,0,0,0,0,0,0,0,0};
+
+ 
+void TIM3_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+
+	if(left_step[1]){
+		current_angle[1]+= delta_angle[1];
+		--left_step[1];
+	}
+	if(left_step[3]){
+		current_angle[3]+= delta_angle[3];
+		--left_step[3];
+	}
+	if(left_step[5]){
+		current_angle[5]+= delta_angle[5];
+		--left_step[5];
+	}
+	if(left_step[7]){
+		current_angle[7]+= delta_angle[7];
+		--left_step[7];
+	}
+    // minimum high of 600 us for -90 degrees, with +90 degrees at 2400 us, 10 us per degree
+    //  timer timebase set to us units to simplify the configuration/math
+    TIM3->CCR1 = 600 + ((current_angle[1] + 90) * 10); // where angle is an int -90 to +90 degrees, PC.6
+    TIM3->CCR2 = 600 + ((current_angle[3] + 90) * 10); // where angle is an int -90 to +90 degrees, PC.7
+    TIM3->CCR3 = 600 + ((current_angle[5] + 90) * 10); // where angle is an int -90 to +90 degrees, PC.8
+    TIM3->CCR4 = 600 + ((current_angle[7] + 90) * 10); // where angle is an int -90 to +90 degrees, PC.9
+  }
+}							 
+
+
+
+
+void TIM4_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+	if(left_step[0]){
+		current_angle[0]+= delta_angle[0];
+		--left_step[0];
+	}
+	if(left_step[2]){
+		current_angle[2]+= delta_angle[2];
+		--left_step[2];
+	}
+	if(left_step[4]){
+		current_angle[4]+= delta_angle[4];
+		--left_step[4];
+	}
+	if(left_step[6]){
+		current_angle[6]+= delta_angle[6];
+		--left_step[6];
+	}
+ 
+    // minimum high of 600 us for -90 degrees, with +90 degrees at 2400 us, 10 us per degree
+    //  timer timebase set to us units to simplify the configuration/math
+    TIM4->CCR1 = 600 + ((current_angle[0] + 90) * 10); // where angle is an int -90 to +90 degrees, PB.6
+    TIM4->CCR2 = 600 + ((current_angle[2] + 90) * 10); // where angle is an int -90 to +90 degrees, PB.7
+    TIM4->CCR3 = 600 + ((current_angle[4] + 90) * 10); // where angle is an int -90 to +90 degrees, PB.8
+    TIM4->CCR4 = 600 + ((current_angle[6] + 90) * 10); // where angle is an int -90 to +90 degrees, PB.8
+  }
+}
+
+void TIM5_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM5, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM5, TIM_IT_Update);
+	if(left_step[8]){
+		current_angle[8]+= delta_angle[8];
+		--left_step[8];
+	}
+	if(left_step[9]){
+		current_angle[9]+= delta_angle[9];
+		--left_step[9];
+	}
+ 
+    // minimum high of 600 us for -90 degrees, with +90 degrees at 2400 us, 10 us per degree
+    //  timer timebase set to us units to simplify the configuration/math
+    TIM5->CCR1 = 600 + ((current_angle[8] + 90) * 10); // where angle is an int -90 to +90 degrees, PB.6
+    TIM5->CCR2 = 600 + ((current_angle[9] + 90) * 10); // where angle is an int -90 to +90 degrees, PB.7
+  }
+}
+
+void delay_halfsecond(u16 n)
+{
+	u16 i;
+	for(i=0;i<n;i++)
+		delay_ms(500);
+}
+
+void smooth(int channel, volatile float target, u32 lefttime)
+{
+		delta_angle[channel]=(target-current_angle[channel])/(lefttime/20);
+		left_step[channel]=	(lefttime/20);	
+}
+void rough(int channel, volatile float target)
+{
+	left_step[channel]=0;
+	current_angle[channel]=target;
+}
+
+void test()
+{
+	current_angle[0]=10;
+	current_angle[1]=-10;
+	current_angle[2]=20;
+	current_angle[3]=-20;
+	current_angle[4]=30;
+	current_angle[5]=-30;
+	current_angle[6]=40;
+	current_angle[7]=-40;
+	current_angle[8]=50;
+	current_angle[9]=-50;
+}
+
+
+ 
+void standsteady()
+{
+	current_angle[0]=0;
+	current_angle[1]=0;
+	current_angle[2]=0;
+	current_angle[3]=0;
+	current_angle[4]=0;
+	current_angle[5]=0;
+	current_angle[6]=0;
+	current_angle[7]=0;
+	current_angle[8]=0;
+	current_angle[9]=45;
+}
+
+
+
+int main(void)
+{
+  delay_init(72);
+  RCC_Configuration();
+  GPIO_Configuration();
+  NVIC_Configuration();
+  TIM3_Configuration();
+  standsteady();
+	
+	
+	//rough(1,20);	  
+	//smooth(1,40,2000);
+	//smooth(3,40,2000);
+	//smooth(5,40,2000);
+	//smooth(7,40,2000);
+
+  while(1)
+  {
+		/*if(p==3000000)
+		{
+			if(sign)
+			{
+				servo_angle[0]=servo_angle[1]=servo_angle[2]=servo_angle[3]=-60;
+				sign=0;
+			}else
+			{
+				servo_angle[0]=servo_angle[1]=servo_angle[2]=servo_angle[3]=0;
+				sign=1;
+			}
+			p=0;
+		}
+		p++;
+		*/
+  }
+}
+ 
+/**************************************************************************************/
+ 
+void RCC_Configuration(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB| RCC_APB2Periph_GPIOC |RCC_APB2Periph_GPIOD| RCC_APB2Periph_AFIO, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+}
+ 
+/**************************************************************************************/
+ 
+void NVIC_Configuration(void)
+{
+   NVIC_InitTypeDef NVIC_InitStructure;
+ 
+   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+ 
+   NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+   NVIC_Init(&NVIC_InitStructure);
+
+   NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+   NVIC_Init(&NVIC_InitStructure);
+   
+   NVIC_InitStructure.NVIC_IRQChannel = TIM5_IRQn;
+   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+   NVIC_Init(&NVIC_InitStructure);
+
+}
+ 
+/**************************************************************************************/
+ 
+void GPIO_Configuration(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+	// PC.06 TIM3_CH1
+	// PC.07 TIM3_CH2
+	// PC.08 TIM3_CH3
+	// PC.09 TIM3_CH4
+
+ 	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 |GPIO_Pin_7 |GPIO_Pin_8 |GPIO_Pin_9;
+ 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_PinRemapConfig(GPIO_FullRemap_TIM3,ENABLE);
+	
+	//	PB.06 TIM4_CH1
+	//	PB.07 TIM4_CH1
+	//	PB.08 TIM4_CH1
+	//	PB.09 TIM4_CH1
+
+
+
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 |GPIO_Pin_7 |GPIO_Pin_8 |GPIO_Pin_9;
+ 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0 |GPIO_Pin_1;
+ 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+
+}
+ 
+/**************************************************************************************/
+ 
+void TIM3_Configuration(void)
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+    TIM_TimeBaseStructure.TIM_Prescaler = 72 - 1;  // 24 MHz / 24 = 1 MHz
+    TIM_TimeBaseStructure.TIM_Period = 20000 - 1; // 1 MHz / 20000 = 50 Hz (20 ms)
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+
+
+  // Set up 4 channel servo
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 600 + 900; // 1500 us - Servo Top Centre
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC1Init(TIM3, &TIM_OCInitStructure);    // Channel 2 configuration = PC.06 TIM3_CH1
+    TIM_OC2Init(TIM3, &TIM_OCInitStructure);    // Channel 4 configuration = PC.07 TIM3_CH2
+    TIM_OC3Init(TIM3, &TIM_OCInitStructure);    // Channel 6 configuration = PC.08 TIM3_CH3
+    TIM_OC4Init(TIM3, &TIM_OCInitStructure);    // Channel 8 configuration = PC.09 TIM3_CH4
+
+	
+	TIM_OC1Init(TIM4, &TIM_OCInitStructure);    // Channel 1 configuration = PD.12 TIM4_CH1
+    TIM_OC2Init(TIM4, &TIM_OCInitStructure);    // Channel 3 configuration = PD.13 TIM4_CH2
+    TIM_OC3Init(TIM4, &TIM_OCInitStructure);    // Channel 5 configuration = PD.14 TIM4_CH3
+    TIM_OC4Init(TIM4, &TIM_OCInitStructure);	// Channel 7 configuration = PD.15 TIM4_CH4
+
+	TIM_OC1Init(TIM5, &TIM_OCInitStructure);    // Channel 1 configuration = PD.12 TIM5_CH1
+    TIM_OC2Init(TIM5, &TIM_OCInitStructure);    // Channel 3 configuration = PD.13 TIM5_CH2
+
+    // turning on TIM3 and PWM outputs
+ 
+	TIM_Cmd(TIM3, ENABLE);
+    TIM_CtrlPWMOutputs(TIM3, ENABLE);
+  	// TIM IT enable
+  	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+
+    // turning on TIM4 and PWM outputs	
+	TIM_Cmd(TIM4, ENABLE);
+    TIM_CtrlPWMOutputs(TIM4, ENABLE);
+  	// TIM IT enable
+  	TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+		
+	TIM_Cmd(TIM5, ENABLE);
+    TIM_CtrlPWMOutputs(TIM5, ENABLE);
+  	// TIM IT enable
+  	TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE);
+}
